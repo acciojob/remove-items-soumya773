e03c342b02cd3e26085cba9389673b9fa34a36de
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-05-06 11:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+      const selectElement = document.getElementById("colorSelect");
+      const selectedIndex = selectElement.selectedIndex;
+
+      if (selectedIndex !== -1) {
+        selectElement.remove(selectedIndex);
+      }
+    }
